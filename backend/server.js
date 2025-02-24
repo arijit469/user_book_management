e6b11bef564d2b7e0010d9998e189070139c7706
unknown <arijitmalik469@gmail.com>
2025-02-24 07:51:04 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use(express.json());
 // mongoose.connect(process.env.MONGO_URI)
 //   .then(() => console.log('MongoDB connected'))
 //   .catch(err => console.error('MongoDB connection error:', err));
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
